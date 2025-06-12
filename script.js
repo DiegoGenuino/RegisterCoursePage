588ef91bbecb21a5a94f23521c08d56b61c4c412
Willian <willianbrunomata@gmail.com>
2025-06-12 17:22:29 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+// Get DOM elements
+const modal = document.getElementById('unitModal');
+const addUnitBtn = document.getElementById('addUnitBtn');
+const closeBtn = document.getElementsByClassName('close')[0];
+const addMoreBtn = document.getElementById('addMoreBtn');
+const finishBtn = document.getElementById('finishBtn');
+const unitsList = document.getElementById('unitsList');
+
+// Array to store curriculum units
+let curriculumUnits = [];
+
+// Open modal
+addUnitBtn.onclick = function () {
+    modal.style.display = 'block';
+    clearModalInputs();
+}
+
+// Close modal
+closeBtn.onclick = function () {
+    modal.style.display = 'none';
+}
+
+// Close modal when clicking outside
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+
+// Add more units
+addMoreBtn.onclick = function () {
+    addUnit();
+    clearModalInputs();
+}
+
+// Finish adding units
+finishBtn.onclick = function () {
+    addUnit();
+    modal.style.display = 'none';
+}
+
+// Function to add a unit
+function addUnit() {
+    const unitName = document.getElementById('unitName').value;
+    const unitWorkload = document.getElementById('unitWorkload').value;
+
+    if (unitName && unitWorkload) {
+        curriculumUnits.push({
+            name: unitName,
+            workload: unitWorkload
+        });
+        updateUnitsList();
+    }
+}
+
+// Function to update the units list in the UI
+function updateUnitsList() {
+    unitsList.innerHTML = '';
+    curriculumUnits.forEach((unit, index) => {
+        const unitElement = document.createElement('div');
+        unitElement.className = 'unit-item';
+        unitElement.innerHTML = `
+            <span>${unit.name}</span>
+            <span class="workload">${unit.workload}h</span>
+        `;
+        unitsList.appendChild(unitElement);
+    });
+}
+
+// Function to clear modal inputs
+function clearModalInputs() {
+    document.getElementById('unitName').value = '';
+    document.getElementById('unitWorkload').value = '';
+}
